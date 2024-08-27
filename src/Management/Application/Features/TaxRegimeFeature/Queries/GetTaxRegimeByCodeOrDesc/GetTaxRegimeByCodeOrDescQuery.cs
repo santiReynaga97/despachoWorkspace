af970fe2c61b5a1ee6420495c1ff7407096f956d
Rodrigo Maldonado <rodrigo.maldonado@tecnoavance.mx>
2024-08-27 11:10:32 -0300

    actualizamos branch

--- src/Management/Application/Features/TaxRegimeFeature/Queries/GetTaxRegimeByCodeOrDesc/GetTaxRegimeByCodeOrDescQuery.cs
+++ src/Management/Application/Features/TaxRegimeFeature/Queries/GetTaxRegimeByCodeOrDesc/GetTaxRegimeByCodeOrDescQuery.cs
@@ -1,12 +1,17 @@
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
 
-namespace Application.Features.TaxRegimeFeature.Queries.GetTaxRegimeByCodeOrDesc
+using Application.Features.TaxRegimeFeature.Queries.GetTaxRegimeByCodeOrDesc;
+using DespachoWorkspace.Management.Application.Common.Models.Results;
+
+namespace DespachoWorkspace.Management.Application.Features.TaxRegimeFeature.Queries.GetTaxRegimeByCodeOrDesc;
+
+public class GetTaxRegimeByCodeOrDescQuery : IRequest<IDataResult<List<GetTaxRegimeByCodeOrDescResponse>>>
 {
-    public class GetTaxRegimeByCodeOrDescQuery
+    public string Code { get; private set; }
+    public string Description { get; private set; }
+
+    public GetTaxRegimeByCodeOrDescQuery(string code, string description)
     {
-        
+        Code = code;
+        Description = description;
     }
 }
